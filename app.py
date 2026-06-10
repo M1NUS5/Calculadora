c667e99c7d52163a9058d4d2473b5cbae922d7b0
M1NUS5 <jtorresalvarez124@gmail.com>
2026-06-10 11:47:24 -0600

    Proyecto calculadora con GitHub Actions

---
+++ app.py
@@ -0,0 +1,6 @@
+from calculator import sumar, restar, multiplicar, dividir
+
+print("sumar", sumar(10, 5))
+print("restar", restar(10, 5))
+print("multiplicar", multiplicar(10, 5))
+print("dividir", dividir(10, 5))
